c9c69375dd0e05569d4a570a02f7ec0ae71b442f
CodeJeffreyG <99752992+CodeJeffreyG@users.noreply.github.com>
2025-01-02 12:17:13 -0500

    ai works

--- Assets/Script/PaddleMovement.cs
+++ Assets/Script/PaddleMovement.cs
@@ -3,12 +3,16 @@ using UnityEngine.InputSystem;
 
 public class PaddleMovement : MonoBehaviour
 {
-    public float paddleSpeed;
-    [SerializeField] Transform transform;
+    public float movementSpeed;
+
+    public Transform transform;
+    public Rigidbody2D rigidBody;
+    public Rigidbody2D leftRigidBody;
+    public Rigidbody2D rightRigidBody;
+    public Rigidbody2D ballRigidBody;
     private bool isRightPaddle;
-    private bool isPressingUp;
-    private bool isPressingDown;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool isRightAI = true;
+    private bool isLeftAI;
     void Start()
     {
         isRightPaddle = transform.localPosition.x < 0;
@@ -22,12 +26,17 @@ public class PaddleMovement : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.W))
             {
-                transform.Translate(Vector2.up * Time.deltaTime * paddleSpeed);
+                isRightAI = true;
+                runAI(rightRigidBody);
+                isLeftAI = false;
+
+                transform.Translate(Vector2.up * Time.deltaTime * movementSpeed);
             }
             else if (Input.GetKey(KeyCode.S))
             {
-                transform.Translate(Vector2.down * Time.deltaTime * paddleSpeed);
+                transform.Translate(Vector2.down * Time.deltaTime * movementSpeed);
             }
+
 
 
 
@@ -36,12 +45,54 @@ public class PaddleMovement : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                transform.Translate(Vector2.up * Time.deltaTime * paddleSpeed);
+                isLeftAI = true;
+                runAI(leftRigidBody);
+                isRightAI = false;
+                transform.Translate(Vector2.up * Time.deltaTime * movementSpeed);
             }
             else if (Input.GetKey(KeyCode.DownArrow))
             {
-                transform.Translate(Vector2.down * Time.deltaTime * paddleSpeed);
+                transform.Translate(Vector2.down * Time.deltaTime * movementSpeed);
             }
+
+        }
+
+        if (isLeftAI)
+        {
+            runAI(leftRigidBody);
+        }
+
+        if (isRightAI)
+        {
+            runAI(rightRigidBody);
+        }
+
+
+
+    }
+
+
+    float findBallYPosition()
+    {
+        return ballRigidBody.position.y;
+    }
+
+    void runAI(Rigidbody2D rigidBody)
+    {
+        float currenetPosition = rigidBody.position.y;
+
+        if (currenetPosition > findBallYPosition())
+        {
+            rigidBody.linearVelocity = new Vector2(0, -movementSpeed);
+        }
+        else if (currenetPosition < findBallYPosition())
+        {
+            rigidBody.linearVelocity = new Vector2(0, movementSpeed);
+        }
+        else if (currenetPosition == findBallYPosition())
+        {
+            return;
         }
     }
 }
+
