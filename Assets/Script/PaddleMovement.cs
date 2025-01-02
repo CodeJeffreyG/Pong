19f45687b9bca3b4bd2a2a6d415c25ac0f621f48
CodeJeffreyG <99752992+CodeJeffreyG@users.noreply.github.com>
2025-01-02 10:42:47 -0500

    logic for right and left movement

--- Assets/Script/PaddleMovement.cs
+++ Assets/Script/PaddleMovement.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PaddleMovement : MonoBehaviour
 {
+    public float paddleSpeed;
+    [SerializeField] Transform transform;
+    private bool isRightPaddle;
+    private bool isPressingUp;
+    private bool isPressingDown;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        isRightPaddle = transform.localPosition.x > 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isRightPaddle)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector2.up * Time.deltaTime * paddleSpeed);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector2.down * Time.deltaTime * paddleSpeed);
+            }
+
+
+
+        }
+        else
+        {
+
+        }
     }
 }
