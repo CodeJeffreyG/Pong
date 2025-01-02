7722209393197a3485db76a0ed1365c483bcd86b
CodeJeffreyG <99752992+CodeJeffreyG@users.noreply.github.com>
2025-01-02 09:53:07 -0500

    randomly choose starting dir

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -4,10 +4,19 @@ public class Ball : MonoBehaviour
 {
 
     public Rigidbody2D rigidBody;
+    public float startingSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        bool isRight = Random.value >= 0.5f;
+        float xVelocity = -1;
+        float yVelocity = Random.Range(-1, 1);
+        if (isRight)
+        {
+            xVelocity = 1;
+        }
 
+        rigidBody.linearVelocity = new Vector2(xVelocity, yVelocity);
     }
 
     // Update is called once per frame
