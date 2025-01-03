5a214808bd722f18cbad3a42b2aec741696396e0
CodeJeffreyG <99752992+CodeJeffreyG@users.noreply.github.com>
2025-01-03 18:35:38 -0500

    created score class to handle score

---
+++ Assets/Script/Score.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public Rigidbody2D rbBall;
+    public Rigidbody2D rbLeftGoal;
+    public Rigidbody2D rbRightGoal;
+
+    public float leftScore;
+    public float rightScore;
+
+
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
